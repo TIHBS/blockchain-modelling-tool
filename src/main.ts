e8866501b672f847bff5d26f0100f98b36fa8420
Fabian Bühler <17296905+buehlefs@users.noreply.github.com>
2020-02-09 20:40:41 +0100

    Add grapheditor

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import {Aurelia} from 'aurelia-framework'
 import * as environment from '../config/environment.json';
 import {PLATFORM} from 'aurelia-pal';
 
+import '@ustutt/grapheditor-webcomponent';
+
 export function configure(aurelia: Aurelia) {
   aurelia.use
     .standardConfiguration()
